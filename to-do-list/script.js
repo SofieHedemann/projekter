43639367ff1911478540b9afae0df4c609d96b38
SofieHedemann <sofhedemann@gmail.com>
2025-09-24 16:03:09 +0200

    blobski another update on the styling; more coherent and similar coloring

--- to-do-list/script.js
+++ to-do-list/script.js
@@ -1,4 +1,5 @@
 const checkbox = document.querySelectorAll('.checkbox');
 const toDoInput = document.querySelectorAll('.toDoInput');
 const checkedOffItems = document.querySelector('.checkedOffItems');
+const addButton = document.querySelector('.addButton');
 
