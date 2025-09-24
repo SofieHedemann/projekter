cd1119a4b5fa252b4c7c4613da10f4ee4e698416
SofieHedemann <sofhedemann@gmail.com>
2025-09-24 11:50:17 +0200

    style updated on to do list

---
+++ to-do-list/script.js
@@ -0,0 +1,4 @@
+const checkbox = document.querySelectorAll('.checkbox');
+const toDoInput = document.querySelectorAll('.toDoInput');
+const checkedOffItems = document.querySelector('.checkedOffItems');
+
